cc3f73f3bbf39673a390e8f7688e77a04ce6e7d1
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2025-01-14 22:20:32 -0500

    completed header routing and styling

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,5 +1,4 @@
 import Header from "../../components/Header/Header";
-import PlantSVG from "../../components/PlantSVG/PlantSVG";
 import ProductCard from "../../components/ProductCard/ProductCard";
 
 export default function ProductsPage() {
