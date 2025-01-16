ce99e1acca26cad056de4fce471e4dca40dbce59
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2025-01-16 00:34:39 -0500

    got data from backend

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,23 +1,30 @@
 import ProductCard from "../../components/ProductCard/ProductCard";
+const baseURL = import.meta.env.VITE_API_URL;
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./ProductsPage.scss";
 
 export default function ProductsPage() {
-  const product = {
-    id: 1,
-    type: "Cleanser",
-    time: "Day",
-    concern: "Dryness",
-    brand: "CeraVe",
-    name: "Hydrating Cleanser",
-    ingredient: "Ceramides",
-    price: 19.99,
-    photo: "images/cerave-cleanser.webp",
-    photoDescription: "image of product",
-    link: "https://www.walmart.ca/en/ip/cerave-hydrating-facial-cleanser-with-hyaluronic-acid-and-3-ceramides-gentle-moisturizing-non-foaming-facial-cleanser-for-men-women-daily-face-wash-fo/6000200706743",
-  };
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(`${baseURL}/products`);
+        console.log(response.data);
+        setProducts(response.data);
+      } catch (error) {
+        console.log(`Error retrieving products: ${error}`);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   return (
-    <div>
-      <ProductCard product={product} />
+    <div className="product-card">
+      {products.map((product) => {
+        return <ProductCard key={product.id} product={product} />;
+      })}
     </div>
   );
 }
